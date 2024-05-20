842963e86a6d4761a30b8b226bb872194ad6604b
Ogaitw <tiagoaugusto2402@gmail.com>
2024-05-19 22:31:15 -0300

    Estruturação.

---
+++ src/main/java/com/buddies/movies/MoviesBuddiesApplication.java
@@ -0,0 +1,13 @@
+package com.buddies.movies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviesBuddiesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoviesBuddiesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/buddies/movies/MoviesBuddiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buddies.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesBuddiesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
